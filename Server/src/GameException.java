d2b67755397769d4df309e32a8364a8de2451a8f
Shohail Parwej <shohailparwej@gmail.com>
2024-02-08 23:21:07 +0530

    first commit

---
+++ Server/src/GameException.java
@@ -0,0 +1,22 @@
+
+public class GameException extends Exception {
+}
+
+class PositionAlreadyMarkedException extends GameException {
+}
+
+class GameDrawException extends GameException {
+}
+
+class PlayerWonException extends GameException {
+    String pos;
+
+    PlayerWonException(String pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public String toString() {
+        return "pos='" + pos;
+    }
+}
